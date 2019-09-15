59d3894e633350956966803e99e854dbc5b226ce
AlexStarodubtsev <starodub97@yandex.ru>
2019-09-15 16:23:49 +0300

    Final Commit

---
+++ NmarketTestTask/Parsers/ExcelParser.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ClosedXML.Excel;
+using NmarketTestTask.Models;
+
+namespace NmarketTestTask.Parsers
+{
+    public class ExcelParser : IParser
+    {
+        public IList<House> GetHouses(string path)
+        {
+            XLWorkbook wb = new XLWorkbook(path);
+            IXLWorksheet sheet = wb.Worksheets.First();
+            IXLRange RangeSheet = sheet.RangeUsed();
+            int rightBorder = RangeSheet.LastColumn().ColumnNumber(); //установим правую границу данных
+            int downBorder = RangeSheet.LastRow().RowNumber(); //установим нижнюю границу данных
+            List<IXLCell> Xlhouses = sheet.Cells().Where(c => c.GetValue<string>().Contains("Дом")).ToList(); //получим ячейки, в которых номера домов
+            List<House> houses = new List<House>();
+            foreach (var Xlhouse in Xlhouses)
+            {
+                House house = new House();
+                house.Name = Xlhouse.GetValue<string>();
+                house.Flats = new List<Flat>();
+                IXLCell cell = Xlhouse.CellBelow();
+                
+                while (   !(    cell.GetValue<string>() == ""
+                             && cell.CellBelow().GetValue<string>() == ""
+                             && cell.CellRight().GetValue<string>() == "" 
+                             && cell.CellRight().CellBelow().GetValue<string>() == "" 
+                           ) 
+                       && cell.WorksheetColumn().ColumnNumber() <= rightBorder
+                      )       //рассматриваем, есть ли значение в ячейке под названием дома, а также в ближайших от нее соседей
+                {
+
+                    IXLCell cellBellowHouse  = cell;
+                    while (   !(  cell.GetValue<string>() == "" 
+                               && cell.CellBelow().GetValue<string>() == ""
+                               ) 
+                             && cell.WorksheetRow().RowNumber() <= downBorder
+                          ) //смотрим есть ли в текущей ячейке значение или той, которая находится снизу
+                     {
+
+                        if (cell.GetValue<string>().Contains("№")) 
+                        {
+                            Flat flat = new Flat();
+                            flat.Number = cell.GetValue<string>().Substring(1);
+                            flat.Price = cell.CellBelow().GetValue<string>();
+                            house.Flats.Add(flat);
+                        }
+                        cell = cell.CellBelow();
+                    }
+
+                    cell = cellBellowHouse.CellRight(); //после того, как закончили с колонкой, переходим в следующую колонку
+                                                        //самую верхнюю под номером дома
+                }
+
+                houses.Add(house);
+            }
+
+            return houses;
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ NmarketTestTask/Parsers/SiteParser.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using HtmlAgilityPack;
+using NmarketTestTask.Models;
+
+namespace NmarketTestTask.Parsers
+{
+    public class SiteParser : IParser
+    {
+        public IList<House> GetHouses(string path)
+        {
+            var doc = new HtmlDocument();
+            doc.Load(path);
+
+            HtmlNode node = doc.DocumentNode.SelectSingleNode("//thead");
+            string text = node.InnerText;
+            HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes(".//th");
+
+            node = doc.DocumentNode.SelectSingleNode("//tbody");
+
+            List<House> Houses = new List<House>();
+            foreach (var tr in node.ChildNodes)
+            {
+
+                House CurrentHouse = new House();
+                Flat CurrentFlat = new Flat();
+
+                foreach (var td in tr.ChildNodes)
+                {
+                    string test = td.GetAttributeValue("class", String.Empty);
+                    switch (td.GetAttributeValue("class", String.Empty))
+                    {
+                        case "house":
+                            foreach (var house in Houses)
+                            {
+                                if (house.Name == td.InnerText.Trim())
+                                {
+                                    CurrentHouse = house;
+                                    break;
+                                }
+
+                            }
+                            if (CurrentHouse.Name is null)
+                            {
+                                CurrentHouse.Name = td.InnerText.Trim();
+                                Houses.Add(CurrentHouse);
+                            }
+                            break;
+
+                        case "number":
+                            CurrentFlat.Number = td.InnerText.Trim();
+                            break;
+
+                        case "price":
+                            CurrentFlat.Price = td.InnerText.Trim();
+                            break;
+                    }
+                    
+                }
+                if (CurrentHouse.Flats == null && !(CurrentFlat.Number is null))
+                {
+                    CurrentHouse.Flats = new List<Flat>();
+                }
+                if (!(CurrentFlat.Number is null))
+                  CurrentHouse.Flats.Add(CurrentFlat);
+            }
+            
+            return Houses;
+            //throw new System.NotImplementedException();
+        }
+    }
+}
